7b2515ec37599f8c257a40a0a04351c5ed04f450
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-01-07 21:38:22 -0600

    use a menu for the new button

--- addons/stageplay/editor.gd
+++ addons/stageplay/editor.gd
@@ -6,6 +6,7 @@ const NodeHelpers := preload("res://addons/stageplay/node_helpers.gd")
 
 @onready var lines: Control = %Lines
 
+
 static func create() -> StagePlayEditor:
 	return preload("res://addons/stageplay/editor.tscn").instantiate()
 
@@ -16,15 +17,45 @@ func unpack(unpacker: Unpacker) -> void:
 		_add_line().unpack(item)
 
 
+func _unpack_from_template() -> void:
+	unpack(Unpacker.from(load("res://addons/stageplay/template.gd").get_template_data()))
+
+
 func _add_line() -> LineEditor:
 	var line := LineEditor.create()
 	lines.add_child(line)
 	return line
 
 
+func _ready() -> void:
+	_setup_new_menu()
+
+
+func _setup_new_menu() -> void:
+	var items := [
+		{
+			text = "Blank",
+			action = func(): unpack(Unpacker.from({ lines = [] })),
+		},
+		{
+			text = "From Template",
+			action = _unpack_from_template,
+		},
+	]
+
+	var popup: PopupMenu = %NewMenu.get_popup()
+
+	for index in popup.get_item_count():
+		popup.remove_item(0)
+
+	for item in items:
+		popup.add_item(item["text"])
+
+	popup.index_pressed.connect(
+		func (index: int) -> void:
+			items[index]["action"].call()
+	)
+
+
 func _on_add_line_button_pressed() -> void:
 	_add_line().speaker_field.input_node.grab_focus()
-
-
-func _on_new_from_template_button_pressed() -> void:
-	unpack(Unpacker.from(load("res://addons/stageplay/template.gd").get_template_data()))
